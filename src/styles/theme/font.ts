cd1fcf9c37aa0f40b5d630a05af882aea0ccf074
Josanghyeon <BStkdgus.dev@gmail.com>
2023-01-26 14:23:56 +0900

    (#3) feat: Theme객체에 font, color, shadow추가

--- src/styles/theme/font.ts
+++ src/styles/theme/font.ts
@@ -1,1 +1,22 @@
-const font = {};
+const fontToCss = (sz: number, wt: number) =>
+  `
+  font-weight: ${wt};
+  font-size: ${sz}px;
+`;
+
+export const font = {
+  heading1: fontToCss(27, 900),
+  heading2: fontToCss(17, 700),
+  heading3: fontToCss(14, 700),
+  heading4: fontToCss(13, 400),
+  title1: fontToCss(23, 700),
+  title2: fontToCss(19, 600),
+  title3: fontToCss(16, 500),
+  title4: fontToCss(15, 500),
+  body1: fontToCss(16, 400),
+  body2: fontToCss(14, 400),
+  body3: fontToCss(13, 400),
+  body4: fontToCss(12, 400),
+};
+
+export type keyOfFont = keyof typeof font;
--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -1,1 +1,5 @@
-export const theme = { color, font };
+import { font } from './font';
+import { color } from './color';
+import { shadow } from './shadow';
+
+export const theme = { color, font, shadow };
