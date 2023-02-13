b3613d0a3edf92edc42419f6993cb1e87f030d20
ipiyou <tao710803@gmail.com>
2023-02-14 03:02:15 +0900

    (#21) feat: error number, name, content, image 추가

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,19 +1,79 @@
 import { Warning } from '@/assets';
 import { Button } from '@/components/common/button';
 import { Text } from '@/components/common/text';
+import { ColumnCenterGap, ColumnGap } from '@/layouts/DirectionGap';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-export const NotFoundPage = () => {
+interface PropsType {
+  Error: {
+    name: string;
+    number: 401 | 403 | 404;
+    content: string;
+    image: string;
+  };
+}
+
+export const NotFoundPage = ({
+  Error: { name, number, content, image },
+}: PropsType) => {
   return (
-    <div>
-      <Warning width={80} height={80} />
-      <Text size="heading1" color="errorDarken1">
-        404 NOT FOUND
-      </Text>
-      <Text size="title3">
-        해당 페이지에 대한 접근권한이 없거나 페이지가 존재하지 않습니다. URL
-        주소를 다시 한번 확인해 주세요.
-      </Text>
-      <Button>이전 화면</Button>
-    </div>
+    <_Wrapper>
+      <_Content>
+        <ColumnGap gap="35px">
+          <_TitleContent>
+            <Text size="title2" color="gray6">
+              {name}
+            </Text>
+            <_ErrorNumber>{number}</_ErrorNumber>
+          </_TitleContent>
+          <_ErrorContent size="title1" lineHeight="37.5px">
+            {content}
+            {`\n아래 버튼을 누르면 홈으로 돌아갈 수 있습니다.`}
+          </_ErrorContent>
+          <Link to="/">
+            <Button>Home</Button>
+          </Link>
+        </ColumnGap>
+      </_Content>
+      <_BackGroundImg src={image} />
+    </_Wrapper>
   );
 };
+
+const _Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  overflow-y: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const _Content = styled.div`
+  width: 416px;
+  margin: auto;
+`;
+
+const _TitleContent = styled.div`
+  width: 210px;
+  border-bottom: 4px solid ${({ theme }) => theme.color.gray10};
+`;
+
+const _ErrorNumber = styled.div`
+  font-size: 120px;
+  font-weight: 900;
+  margin-bottom: 30px;
+`;
+
+const _ErrorContent = styled(Text)`
+  white-space: pre-wrap;
+`;
+
+const _BackGroundImg = styled.img`
+  width: 50%;
+  object-fit: cover;
+  @media screen and (max-width: 1200px) {
+    display: none;
+  }
+`;
