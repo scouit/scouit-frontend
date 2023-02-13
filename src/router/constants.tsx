59d55e70673df83eb7e425832cbfdf69137d9c53
ipiyou <tao710803@gmail.com>
2023-02-14 02:54:48 +0900

    (#21) feat: 각 에러 별 경로, 상태 추가

--- src/router/constants.tsx
+++ src/router/constants.tsx
@@ -1,3 +1,5 @@
+import { Error401, Error403, Error404 } from '@/assets';
+
 export const routePath = {
   home: '/',
   signin: '/sign-in',
@@ -25,4 +27,27 @@ export const routePath = {
   },
   myPage: '/my-page',
   notFound: '/*',
+  unAuth: '/401',
+  forbid: '/403',
+} as const;
+
+export const ErrorState = {
+  unfind404: {
+    name: 'Not Found',
+    content: `Sorry, you are not authorized\n to access this page.`,
+    image: Error404,
+    number: 404,
+  },
+  unAuth401: {
+    name: 'Unauthorized',
+    content: '이 페이지에 대한 접근권한이 존재하지 않습니다.',
+    image: Error401,
+    number: 401,
+  },
+  forbid403: {
+    name: 'Forbidden',
+    content: '이런, 페이지를 찾을 수 없습니다.',
+    image: Error403,
+    number: 403,
+  },
 } as const;
