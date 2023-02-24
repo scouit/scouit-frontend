10917a3877b75a31a2d72e710d94a5e9030241ea
ipiyou <tao710803@gmail.com>
2023-02-25 04:06:23 +0900

    (#30) feat: 각 프로필 작성 페이지

--- src/pages/write/Basic.tsx
+++ src/pages/write/Basic.tsx
@@ -1,39 +1,31 @@
 import { Tapbar } from '@/components/write-profile';
 import { TextArea } from '@/components/textarea';
-import { ProfileWriteForm } from '@/components/profileWriteForm';
+import { ProfileWriteBox } from '@/components/profileWriteForm';
 import { ColumnGap } from '@/layouts/DirectionGap';
 import { useEffect } from 'react';
-import { useBasic, useProfileUpdate } from '@/hooks/useProfile';
+import { useProfileUpdate } from '@/hooks/useProfile';
 import { EditProfileWrapper } from '@/layouts/Wrapper/EditProfile';
+import { Input } from '@/components/common/input';
+import { Header } from '@/components/header';
+import { lio } from '@/components/write-profile/constants';
 
 export const BasicPage = () => {
-  const { profile, handleOnChange } = useBasic();
-
   const { basicUpdate } = useProfileUpdate();
   useEffect(() => {
     return () => basicUpdate.mutate();
   }, []);
   return (
     <>
-      <ProfileWriteForm title="소개">
-        <ColumnGap gap="20px" margin="47px 0 0">
-          <TextArea
-            name="oneLineIntroduction"
-            value={profile.basic.oneLineIntroduction}
-            onChange={handleOnChange}
-            label="한 줄 소개"
-            placeholder="한 줄소개를 입력해주세요"
-          />
-          <TextArea
-            name="aboutMe"
-            value={profile.basic.aboutMe}
-            onChange={handleOnChange}
-            label="자기소개"
-            placeholder="자기소개를 입력해주세요"
-          />
-        </ColumnGap>
-      </ProfileWriteForm>
-      <Tapbar />
+      <Header
+        textList={lio}
+        currentPage="기본 정보"
+        gap="17px"
+        isMedia={true}
+      />
+      <ProfileWriteBox title="기본 정보">
+        <Input name="gas" label="직군" placeholder="" />
+      </ProfileWriteBox>
+      <Tapbar currentPage="기본 정보" />
     </>
   );
 };
