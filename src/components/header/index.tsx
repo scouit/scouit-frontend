9a247350e4892dca20f07f7eaf3395e9563237ef
ipiyou <tao710803@gmail.com>
2023-02-25 04:13:18 +0900

    (#30) feat: 헤더 위로 스크롤시 사라짐

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,6 @@
 import { Hamburger, LogoWithText } from '@/assets';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { Link, useParams } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 import { NavigationList } from '../common/list/Navigation';
 import { Columns } from '../../layouts/Columns';
 import { NavListIsLogin } from './constants';
@@ -8,13 +8,26 @@ import { useRecoilValue } from 'recoil';
 import { profileSelectBox } from '@/store/modal';
 import { ProfileSelectBox } from '../select-box/Profile';
 import { localStorgeGetItem } from '@/utils/storge';
-import { media } from '@/media';
+import { media } from '@/styles/media';
+import { BottomListNav } from './bottom/ListNav';
+import { useHeaderScroll } from '@/hooks/useHeaderScroll';
 
-export const Header = () => {
+export interface BottomHeaderType {
+  textList?: {
+    title: string;
+    url: string;
+  }[];
+  currentPage?: string;
+  gap?: string;
+  isMedia?: boolean;
+}
+
+export const Header = (bottomProps: BottomHeaderType) => {
   const profile = useRecoilValue(profileSelectBox);
+  const display = useHeaderScroll();
   return (
     <_Wrapper>
-      <_Column12>
+      <_Column12 display={display}>
         <Link to={'/'}>
           <LogoWithText />
         </Link>
@@ -30,32 +43,42 @@ export const Header = () => {
         </nav>
       </_Column12>
       {profile && <ProfileSelectBox />}
+      {bottomProps.textList && <BottomListNav {...bottomProps} />}
     </_Wrapper>
   );
 };
 
 const _Wrapper = styled.header`
   position: fixed;
-  right: 0;
+  width: 100%;
+  top: 0;
   left: 0;
-  z-index: 1;
+  z-index: 2;
   border-bottom: 1px solid ${({ theme }) => theme.color.gray4};
   background-color: ${({ theme }) => theme.color.gray1};
+  box-shadow: ${({ theme }) => theme.shadow.modal};
 `;
 
-const _Column12 = styled(Columns)`
+const _Column12 = styled(Columns)<{ display: boolean }>`
+  display: flex;
+  transition: height 0.25s;
   width: 100%;
   max-width: 1152px;
-  display: flex;
   align-items: center;
   justify-content: space-between;
   height: 70px;
   padding: 0 1.5rem;
-  ${media.media720`padding: 0 1rem;`};
+  ${({ display }) =>
+    !display &&
+    css`
+      visibility: hidden;
+      height: 0;
+    `}
+  ${media._720(`padding: 0 1rem;`)};
 `;
 
 const _Hamburger = styled.div`
   cursor: pointer;
   display: none;
-  ${media.media720`display: block;`}
+  ${media._720(`display: block;`)}
 `;
