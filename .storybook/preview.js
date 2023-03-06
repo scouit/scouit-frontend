cc80cf99a2f5215c545c8022c3507fa0d882fd50
Josanghyeon <BStkdgus.dev@gmail.com>
2023-03-06 18:49:16 +0900

    (#34) feat: storybook 추가

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import { StyleProvider } from '../src/styles';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <StyleProvider>
+      <Story />
+    </StyleProvider>
+  ),
+];
