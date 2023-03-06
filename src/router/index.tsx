22d56eea3af1c6c433c936deb2b3fe9ba7243b0b
Josanghyeon <BStkdgus.dev@gmail.com>
2023-03-06 09:23:50 +0900

    (#32) refactor: route import 모두 삭제

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,33 +1,5 @@
-import { BrowserRouter, Route, Routes, matchPath } from 'react-router-dom';
-import MakesPeoplePage from '@/pages/MakesPeople';
-import { ResetPasswordPage } from '@/pages/auth/ResetPassword';
-import { FrequentlyAskedQuestionsPage } from '@/pages/FrequentlyAskedQuestions';
-import HomePage from '@/pages/Home';
-import { JobPostingPage } from '@/pages/company/job-posting';
-import { SignInPage } from '@/pages/auth/SignIn';
-import { NotFoundPage } from '@/pages/NotFound';
-import { ResumeDetailPage } from '@/pages/developers/resume/Detail';
-import { SignUpPage } from '@/pages/auth/SignUp';
-import { JobPostingDetailPage } from '@/pages/company/job-posting/Detail';
-import { MyPage } from '@/pages/MyPage';
-import { BasicPage } from '@/pages/write/Basic';
-import { ProjectPage } from '@/pages/write/Project';
-import { ExperiencePage } from '@/pages/write/Experiment';
-import { EditProfileWrapper } from '@/layouts/wrapper/EditProfile';
-import { IntroducePage } from '@/pages/write/introduce';
-import { TechPage } from '@/pages/write/Tech';
-import ActivePage from '@/pages/write/Active';
-import { EducatePage } from '@/pages/write/Educate';
-import ClubPage from '@/pages/Club';
-import { SideProjectPage } from '@/pages/Side';
-import { AdminHomePage } from '@/pages/admin/Home';
-import { AdminClubPage } from '@/pages/admin/Club';
-import { LoungeHomePage } from '@/pages/lounge/Home';
-import { LoungeWrite } from '@/pages/lounge/Write';
-import { LoungeDetail } from '@/pages/lounge/Detail';
-import { Link } from 'react-router-dom';
-import { useRouteComponent } from '@/hooks/useRouteComponent';
-import { createElement, useEffect, useState, lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState, lazy, Suspense } from 'react';
 
 // 파일경로랑 === path
 // export default
