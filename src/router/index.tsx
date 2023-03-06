1d7867dbc5663b65930f72b46e43e09a1ea3cd34
ipiyou <tao710803@gmail.com>
2023-03-06 22:46:20 +0900

    (#34) feat: 정규식으로 동적 라우팅

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,26 +2,26 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useEffect, useState, lazy, Suspense } from 'react';
 import { PAGE_LIST } from './constants';
 
+const pathRex = /(\[)(:)((\D)+)(\])/g;
+
 export const Router = () => (
   <BrowserRouter>
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         {PAGE_LIST.map((path) => {
+          const componentPath = path.replace(pathRex, '$2$3');
+          const filePath = path.replace(pathRex, '$1$3$5');
           const [Component, setComponent] = useState(null);
           useEffect(() => {
             const importModule = async () => {
-              const component = lazy(() => import(`../pages/${path}`));
+              const component = lazy(() => import(`../pages/${filePath}`));
               setComponent(component);
             };
             importModule();
-          }, [path]);
+          }, [filePath]);
+
           return (
-            Component && (
-              <Route
-                path={path.replace(/(\[)((\D)+)(\])/g, ':$2')}
-                element={<Component />}
-              />
-            )
+            Component && <Route path={componentPath} element={<Component />} />
           );
         })}
       </Routes>
